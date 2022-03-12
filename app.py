ee72d1dc6beea7f9a4739eebc021dc02c723f691
RachelRautenberg <96299861+RachelRautenberg@users.noreply.github.com>
2022-03-11 20:44:50 -0600

    deliverable 1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
+from splinter import Browser
 
-add = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # Use flask_pymongo to set up mongo collection
 
@@ -19,7 +21,7 @@ def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
     mars.update_one({}, {"$set": mars_data}, upsert=True)
-    return rediret('/', code=320)
+    return redirect('/', code=320)
 
-if __name__ = "__main--":
+if __name__ == "__main__":
     app.run()
